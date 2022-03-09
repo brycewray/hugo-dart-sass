0983787a195b868fd1d29d297e9b3cf9f6be1873
Bryce Wray <bwray@wraytx.com>
2022-03-09 12:54:58 -0600

    Trying one from PATH

--- build-to-render.sh
+++ build-to-render.sh
@@ -3,7 +3,7 @@
 echo "Install Dart Sass Embedded..."
 
 # This should be in the PATH.
-BIN_DIR=/opt/build/repo/node_modules/bin
+BIN_DIR=/opt/render/project/src/.venv/bin
 # @bep original was:
 # BIN_DIR=/opt/build/repo/node_modules/.bin
 DARTSASS_VERSION=1.49.9
@@ -31,3 +31,22 @@ dart-sass-embedded --version
 echo "Building..."
 
 hugo
+
+# Path is /opt/render/project/src/.venv/bin:
+# /opt/render/project/python/Python-3.7.10/bin:
+# /opt/render/project/nodes/node-17.6.0/bin:
+# /opt/render/project/src/node_modules/.bin:
+# /opt/render/project/.gems/bin:
+# /opt/render/project/src/.venv/bin:
+# /usr/local/bin/dart-sass:
+# /opt/render/project/go/bin:
+# /go/bin:/usr/local/go/bin:
+# /usr/local/cargo/bin:
+# /opt/render/project/.cargo/bin:
+# /usr/local/sbin:
+# /usr/local/bin:
+# /usr/sbin:
+# /usr/bin:
+# /sbin:/bin 
+
+# pwd output is: /opt/render/project/src
