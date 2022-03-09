afef81f7749caebcae80d6e269bf6d798fd60837
Bryce Wray <bwray@wraytx.com>
2022-03-09 11:43:44 -0600

    Probably last shot (and no chmod this time)

--- build-to-cfp.sh
+++ build-to-cfp.sh
@@ -3,15 +3,15 @@
 echo "Install Dart Sass Embedded..."
 
 # This should be in the PATH.
-BIN_DIR=/usr/local/bin
+BIN_DIR=/opt/buildhome/.binrc/bin
 # @bep original was:
 # BIN_DIR=/opt/build/repo/node_modules/.bin
 DARTSASS_VERSION=1.49.9
 
-echo "Path is $PATH" # per @bep suggestion
+# echo "Path is $PATH"
 
-echo "pwd output is:"
-pwd
+# echo "pwd output is:"
+# pwd
 
 mkdir -p $BIN_DIR
 
