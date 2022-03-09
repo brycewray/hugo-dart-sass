b2563bcd29214148e69fa56e953dc8b159295cc6
Bryce Wray <bwray@wraytx.com>
2022-03-09 08:24:56 -0600

    See if Vercel's PATH is same (both Linux, so...)

--- build-to-vercel.sh
+++ build-to-vercel.sh
@@ -2,10 +2,23 @@
 
 echo "Install Dart Sass Embedded..."
 
+# This is in Vercel's PATH.
+BIN_DIR=/opt/build/repo/node_modules/.bin
 DARTSASS_VERSION=1.49.9
 
-# trying method from https://vercel.com/docs/concepts/deployments/build-step
-amazon-linux-extras install dart-sass-embedded
+mkdir -p $BIN_DIR
+
+curl -LJO https://github.com/sass/dart-sass-embedded/releases/download/${DARTSASS_VERSION}/sass_embedded-${DARTSASS_VERSION}-linux-x64.tar.gz;
+
+tar -xvf sass_embedded-${DARTSASS_VERSION}-linux-x64.tar.gz;
+
+mv sass_embedded/dart-sass-embedded $BIN_DIR
+
+rm -rf sass_embedded*;
+
+echo "List Bin Dir..."
+
+ls $BIN_DIR;
 
 dart-sass-embedded --version
 
