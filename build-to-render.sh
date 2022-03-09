62b6b17c619e39b29580aa7cf07d0ca512c4e9af
Bryce Wray <bwray@wraytx.com>
2022-03-09 12:46:22 -0600

    First try with Render (seeking $PATH info)

---
+++ build-to-render.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+echo "Install Dart Sass Embedded..."
+
+# This should be in the PATH.
+BIN_DIR=/opt/build/repo/node_modules/bin
+# @bep original was:
+# BIN_DIR=/opt/build/repo/node_modules/.bin
+DARTSASS_VERSION=1.49.9
+
+echo "Path is $PATH"
+
+echo "pwd output is:"
+pwd
+
+mkdir -p $BIN_DIR
+
+curl -LJO https://github.com/sass/dart-sass-embedded/releases/download/${DARTSASS_VERSION}/sass_embedded-${DARTSASS_VERSION}-linux-x64.tar.gz;
+
+tar -xvf sass_embedded-${DARTSASS_VERSION}-linux-x64.tar.gz;
+
+mv sass_embedded/dart-sass-embedded $BIN_DIR
+
+rm -rf sass_embedded*;
+
+echo "List Bin Dir..."
+ls $BIN_DIR;
+
+dart-sass-embedded --version
+
+echo "Building..."
+
+hugo
